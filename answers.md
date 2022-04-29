921cc09a0778541bae47f56f6857f2d0e8d4d2e8
Neymar Jr <65818526+HaochenC@users.noreply.github.com>
2022-04-29 17:09:41 +0000

    Haochen

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Assignment 5
 ## Answers
 
-**Name:**_________________________
+**Name:**_______Haochen Chen__________________
 
 
 Place all written answers from `assignment-05.md` here for easier grading.
@@ -11,14 +11,47 @@ Place all written answers from `assignment-05.md` here for easier grading.
 
 
 - **1a.**
+At each time, the algorithm chooses as many as possible the largest denominaiton until N dollars are exchanged.
+The pseudocode is as follows:
 
+cnt_coins = 0
+For i from k to 0:
+    cnt = floor(N/2^i)
+    N -= cnt*2^i
+    cnt_coins += cnt
+return cnt_coins
 
+Proof of the optimality:
+Each N can be rewritten uniquely in base 2 that is N = sum ai 2^i with ai = 0 or 1.
+For i >= k and ai = 1, it is easy to verify that expressing 2^i with the minimum number 2^0, ..., 2^k is to use 2^i/2^k 2^k.
+And for i < k, the greedy algorithm uses ai 2^i.
+Let O = (o0, ..., ok) be the optimal solution.
+Since the N mod 2^k can uniquely be represented by 2^0, ..., 2^k-1, o0, ..., ok-1 are all the same as a0, ..., ak-1, that is the same as the greedy soltuion.
 
+- **1b.**
 
+Work O(N)
+Span O(N)
 
+- **2a.**
 
+Let 1, 4, 6 be the set of coin denominations.
+Suppose we make change for N = 8.
+The greedy solution is 1 6 coin and 2 1 coins, however, the optimal solution is 2 4 coins.
+The reason is that a number N can be expressed in multi-ways but the greedy algorithm only tries one of them.
 
 - **2b.**
 
+The recursive function is minCoins(V) = min{1 + minCoins(V - Di) for 0 <= i <= k}.
+Hence, the dynamic programming algorithm works as follows.
+
+Initalize an array C[N + 1]
+for i from 1 to N:
+    for j from 0 to k:
+        if Dj < i and C[i - Dj] is defined:
+            C[i] = min(C[i], C[i - Dj] + 1)
+return C[N]
 
 
+Work O(kN)
+Span O(N)
--- main.py
+++ main.py
@@ -14,16 +14,50 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
-
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:]))) # done
 
 def fast_MED(S, T, MED={}):
     # TODO -  implement memoization
-    pass
+    if S == "" or T == "":
+        return max(len(T), len(S))
+    else:
+        if (S, T) in MED:
+            return MED[(S, T)]
+        else:
+            if S[0] == T[0]:
+                MED[(S, T)] = fast_MED(S[1:], T[1:])
+            else:
+                MED[(S, T)] = 1 + min(fast_MED(S, T[1:]), fast_MED(S[1:], T), fast_MED(S[1:], T[1:]))
+            return MED[(S, T)]
+
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
-    pass
+    if S == "":
+        MED[(S, T)] = ['-'*len(T), T, len(T)]
+        return '-'*len(T), T
+    if T == "":
+        MED[(S, T)] = [S, '-'*len(S), len(S)]
+        return S, '-'*len(S)
+    else:
+        if (S, T) in MED:
+            return MED[(S, T)][0], MED[(S, T)][1]
+        else:
+            if S[0] == T[0]:
+                X, Y =  fast_align_MED(S[1:], T[1:])
+                MED[(S, T)] = [S[0] + X, T[0] + Y, MED[(S[1:], T[1:])][2]]
+            else:
+                Xadd, Yadd = fast_align_MED(S, T[1:])
+                Xdel, Ydel = fast_align_MED(S[1:], T)
+                Xsub, Ysub = fast_align_MED(S[1:], T[1:])
+                cost = 1 + min(MED[(S, T[1:])][2], MED[(S[1:], T)][2], MED[(S[1:], T[1:])][2])
+                if cost - 1 == MED[(S[1:], T[1:])][2]:
+                    MED[(S, T)] = [S[0] + Xsub, T[0] + Ysub, cost]
+                elif cost - 1 == MED[(S, T[1:])][2]:
+                    MED[(S, T)] = ['-' + Xadd, T[0] + Yadd, cost]
+                else:
+                    MED[(S, T)] = [S[0] + Xdel, '-' + Ydel, cost]
+            return MED[(S, T)][0], MED[(S, T)][1]
 
 def test_MED():
     for S, T in test_cases:
@@ -34,3 +68,6 @@ def test_align():
         S, T = test_cases[i]
         align_S, align_T = fast_align_MED(S, T)
         assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+
+if __name__ == '__main__':
+    test_align()
